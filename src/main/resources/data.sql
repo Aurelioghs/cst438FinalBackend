310999dfd82d681947aab1d125b730d14364a6d4
Kenrick Hlaing <kenrick0102@gmail.com>
2023-11-13 15:40:21 -0800

    Added data to coords table for pre-populated users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,9 @@
 INSERT INTO user_table
-(name, email,password, role,city,state_code,country_code) VALUES
+(name, email, password, role, city, state_code, country_code) VALUES
 ('user','user@csumb.edu','$2a$10$NVM0n8ElaRgg7zWO1CxUdei7vWoPg91Lz2aYavh9.f9q0e4bRadue','USER','Martinez','CA','US'),
 ('admin','admin@csumb.edu','$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW','ADMIN','Monterey','CA','US');
+
+INSERT INTO coords
+(user_id, lat, lon) VALUES
+(1, 36.600258, -121.89464),
+(2, 38.013893, -122.133865);
