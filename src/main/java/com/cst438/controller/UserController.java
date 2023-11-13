4ec2095fa3ae18e86868809c00f3b7beeadbea99
Kenrick Hlaing <kenrick0102@gmail.com>
2023-11-13 11:41:42 -0800

    Commit for /user & /users methods

--- src/main/java/com/cst438/controller/UserController.java
+++ src/main/java/com/cst438/controller/UserController.java
@@ -3,6 +3,7 @@ package com.cst438.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,7 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.client.RestTemplate;
+import org.json.JSONObject;
 
 import com.cst438.domain.CoordinateRepository;
 import com.cst438.domain.User;
@@ -29,28 +31,67 @@ public class UserController {
 	@Autowired
 	CoordinateRepository coordinateRepository;
 
-	// Methods to add a user and get a user by email
+	// Methods:
+	// add a user
+	// get a user by email
+	// get list of all users
+	//
 	
 	// Adds a student
-		@PostMapping("/user")
-		@Transactional
-		public User addUsesr( @RequestBody UserDTO userDto ) {
-			User newUser = new User();
-			newUser.setName(userDto.name());
-			newUser.setEmail(userDto.email());
-			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-			newUser.setPassword(encoder.encode(userDto.password()));
-			newUser.setRole(userDto.role());
-			newUser.setCity(userDto.city());
-			newUser.setState_code(userDto.state_code());
-			newUser.setCountry_code(userDto.country_code());
-			return userRepository.save(newUser);
+	@PostMapping("/user")
+	@Transactional
+	public boolean addUsesr( @RequestBody UserDTO userDto ) {
+		User user = userRepository.findByEmail(userDto.email());
+		if(user != null) {
+			// User is already in system
+			return false;
 		}
+		// User not found in system, make new user
+		User newUser = new User();
+		newUser.setName(userDto.name());
+		newUser.setEmail(userDto.email());
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		newUser.setPassword(encoder.encode(userDto.password()));
+		newUser.setRole(userDto.role());
+		newUser.setCity(userDto.city());
+		newUser.setState_code(userDto.state_code());
+		newUser.setCountry_code(userDto.country_code());
+		// Save new user to repository
+		userRepository.save(newUser);
+		
+		// Save Coordinate for user's entered home
+		String apiKey = "3c68fedb4d4cc2ee43ad218fedc95ec9"; // WeatherApp API Key
+		String geoCode = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
+		String cityName = newUser.getCity();
+		String stateCode = newUser.getState_code();
+		String countryCode = newUser.getCountry_code();
+		String geoCodeEndpoint = "http://api.openweathermap.org/geo/1.0/direct?q=%s,%s,%s&appid=%s";						
+		String geoUrl = String.format(geoCodeEndpoint, cityName, stateCode, countryCode, apiKey);
+		
+		// Create instance of RestTemplate for the HTTP Request
+		RestTemplate restTemplate = new RestTemplate();
+		// Perform a GET request to geoUrl
+		// The response should be a String
+		ResponseEntity<String> response = restTemplate.getForEntity(geoUrl, String.class);
+		String responseBody = response.getBody();
+		System.out.println("Response Body: " + responseBody);
+//		JSONObject jo = new JSONObject(responseBody);
+//		double lat = jo.getDouble("lat");
+//		double lon = jo.getDouble("lon");
+		return true;
+	}
 	
 	// Get User By Email
-		@GetMapping("/user/{email}")
-		public User getUserByEmail(@PathVariable String email) {
-			User user = userRepository.findByEmail(email);
-		    return user;
-		}
+	@GetMapping("/user/{email}")
+	public User getUserByEmail(@PathVariable String email) {
+		User user = userRepository.findByEmail(email);
+		return user;
+	}
+	
+	// Get List of User
+	@GetMapping("/users")
+    public List<User> getAllUsers() {
+        List<User> users = (List<User>) userRepository.findAll();
+        return users;
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
-INSERT INTO user_table VALUES
+INSERT INTO user_table
+(name, email,password, role,city,state_code,country_code) VALUES
 ('user','user@csumb.edu','$2a$10$NVM0n8ElaRgg7zWO1CxUdei7vWoPg91Lz2aYavh9.f9q0e4bRadue','USER','Martinez','CA','US'),
 ('admin','admin@csumb.edu','$2a$10$8cjz47bjbR4Mn8GMg9IZx.vyjhLXR/SKKMSZ9.mP9vpMu0ssKi8GW','ADMIN','Monterey','CA','US');
