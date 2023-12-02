abf580b74a46ca34f4c335c7e68e0b282b8a9473
Kenrick Hlaing <kenrick0102@gmail.com>
2023-12-01 18:39:47 -0800

    Added Query to get all Coordinates by user_id

--- src/main/java/com/cst438/domain/CoordinateRepository.java
+++ src/main/java/com/cst438/domain/CoordinateRepository.java
@@ -1,7 +1,12 @@
 package com.cst438.domain;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CoordinateRepository extends CrudRepository <Coordinate, Integer>{
-
+	@Query("SELECT c FROM Coordinate c WHERE c.user.user_id = :userId")
+	List<Coordinate> findAllByUser(@Param("userId") int userId);
 }
